0ac9390a45bf9c39c19c845984e383ccc579cde7
Thiago Augusto Barros de Almeida <thy-almeida@hotmail.com>
2021-02-15 18:42:01 +1030

    Added Collector Service and Wall Creator Service

---
+++ src/Onbox.Revit.Tests.Sample/Services/CollectorService.cs
@@ -0,0 +1,19 @@
+﻿using Autodesk.Revit.DB;
+using System.Linq;
+
+namespace Onbox.Revit.Tests.Sample.Services
+{
+    public class CollectorService
+    {
+        public ElementId CollectFirstLevelInstance(Document doc)
+        {
+            var level = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_Levels)
+                .WhereElementIsNotElementType()
+                .ToElementIds()
+                .First();
+
+            return level;
+        }
+    }
+}
---
+++ src/Onbox.Revit.Tests.Sample/Services/WallCreatorService.cs
@@ -0,0 +1,21 @@
+﻿using Autodesk.Revit.DB;
+
+namespace Onbox.Revit.Tests.Sample.Services
+{
+    public class WallCreatorService
+    {
+        private readonly CollectorService collector;
+
+        public WallCreatorService(CollectorService collector)
+        {
+            this.collector = collector;
+        }
+
+        public Wall CreateWall(Document doc, Line line)
+        {
+            var levelId = this.collector.CollectFirstLevelInstance(doc);
+            var wall = Wall.Create(doc, line, levelId, false);
+            return wall;
+        }
+    }
+}
