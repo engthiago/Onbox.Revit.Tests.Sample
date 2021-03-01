dd98a92c243f2287e60bfdc4e42b9cb4f1d5ff33
Thiago Augusto Barros de Almeida <thy-almeida@hotmail.com>
2021-03-02 09:43:57 +1030

    Fail test

--- tests/Onbox.Revit.Tests/WallCreatorShould.cs
+++ tests/Onbox.Revit.Tests/WallCreatorShould.cs
@@ -36,6 +36,12 @@ namespace Onbox.Revit.Tests
         }
 
         [Test]
+        public void ThisTestShouldFail()
+        {
+            Assert.AreEqual(true, false);
+        }
+
+        [Test]
         public void ThrowExceptionWhenCreatingOutsideOfTransaction()
         {
             var collector = new CollectorService();
